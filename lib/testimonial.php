0bea664de1ef836d7fdd5107ef1c49cb3cfecfed
DeliciousLayao <mike@leadsnearby.com>
2018-08-20 17:19:19 -0400

    added options for testimonials

--- lib/testimonial.php
+++ lib/testimonial.php
@@ -12,8 +12,32 @@
 			'params'     => array(
 				array(
 					'type'        => 'colorpickeralpha',
-					'heading'     => esc_attr__( 'Select Background Color', 'fusion-builder' ),
-					'param_name'  => 'background_color',
+					'heading'     => esc_attr__( 'Select Stars Background Color', 'fusion-builder' ),
+					'param_name'  => 'stars_background_color',
+					'value'       => '#000',
+				),
+				array(
+					'type'        => 'colorpickeralpha',
+					'heading'     => esc_attr__( 'Select Stars Color', 'fusion-builder' ),
+					'param_name'  => 'stars_color',
+					'value'       => '#fee300',
+				),
+				array(
+					'type'        => 'colorpickeralpha',
+					'heading'     => esc_attr__( 'Select Review Background Color', 'fusion-builder' ),
+					'param_name'  => 'reviewbg',
+					'value'       => '#fee300',
+				),
+				array(
+					'type'        => 'colorpickeralpha',
+					'heading'     => esc_attr__( 'Select Review Text Color', 'fusion-builder' ),
+					'param_name'  => 'textbody',
+					'value'       => '#000',
+				),
+				array(
+					'type'        => 'colorpickeralpha',
+					'heading'     => esc_attr__( 'Select Author Text Color', 'fusion-builder' ),
+					'param_name'  => 'authortext',
 					'value'       => '#000',
 				),
 			),
@@ -26,7 +50,7 @@ function custom_testimonial_shortcode( $atts = [], $content = null ) {
 	
 	ob_start(); ?>
 	
-	 <?php echo do_shortcode('[dyn-test-widget]'); ?>
+	 <?php echo do_shortcode('[dyn-test-widget background='.$atts['stars_background_color'].' stars='.$atts['stars_color'].' reviewbg='.$atts['reviewbg'].' textbody='.$atts['textbody'].' authortext='.$atts['authortext'].']'); ?>
 	<?php return ob_get_clean();
 }
 
